442c19a18a3f2c40a67e99bf1fcf7de8294d246a
yakupbeyoglu <yakupbeyoglu@gmail.com>
2022-11-25 00:59:07 +0200

    -y added for nodejs script

--- src/package/NodeJS.sh
+++ src/package/NodeJS.sh
@@ -8,7 +8,7 @@ fi
 echo "Installing Node js 18.x  please wait"
 apt-get update -y && apt-get -y install curl
 curl -sL https://deb.nodesource.com/setup_18.x | -E bash -
-apt install nodejs 
+apt install nodejs -y
 node -v
 echo "Node js installed successfully"
 exit 0
