f42b5f4e9f339c314820f99a25e3ad801cc6f2fb
yakupbeyoglu <yakupbeyoglu@gmail.com>
2022-11-25 02:26:30 +0200

    Config file added to accept traffic to web file

--- src/package/Apache2.sh
+++ src/package/Apache2.sh
@@ -9,5 +9,7 @@ echo "Installing Apache2"
 
 apt-get update -y && sudo apt -y install apache2
 systemctl status apache2
+a2enmod rewrite && sudo service apache2 restart
+cp ./configs/apache2.conf /etc/apache2/apache2.conf
 echo "Apache2 Install process finish"
 exit 0
